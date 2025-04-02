e6cb8fbfb35b885dd52259a7310948fb7e1a5318
g.vela <g.vela@student.vu.nl>
2025-04-02 13:54:04 +0200

    minor change

--- setup.py
+++ setup.py
@@ -4,25 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gsport", # Replace with your own username
+    name="gsport",  # Replace with your own username
     version="1.6.1",
     author="Niels de Water",
-    install_requires=['requests==2.22.0'],
+    install_requires=["requests==2.22.0"],
     author_email="n.dewater@genomescan.nl",
     description="GSPORT download tool for GenomeScan data from customer portal in command-line interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/genomescan/gsport.git",
-    py_modules=['gsport'],
+    py_modules=["gsport"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    entry_points={
-        "console_scripts": [
-            "gsport = gsport:main"
-        ]
-    },
-    python_requires='>=3.7',
+    entry_points={"console_scripts": ["gsport = gsport:main"]},
+    python_requires=">=3.7",
 )
